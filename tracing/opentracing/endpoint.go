48ec802615d0d674cdbc5cd2894054bb95b62d5c
Alexander Babai <aliaksandr.babai@gmail.com>
2021-03-21 02:31:06 +0300

    refactor(tracing.opentracing): trace business err too

--- tracing/opentracing/endpoint.go
+++ tracing/opentracing/endpoint.go
@@ -24,11 +24,23 @@ func TraceConsumer(tracer opentracing.Tracer, operationName string) endpoint.Mid
 			ctx = opentracing.ContextWithSpan(ctx, span)
 
 			response, err := next(ctx, request)
-			if err != nil {
+			if err := extractError(response, err); err != nil {
 				ext.LogError(span, err)
 			}
 
 			return response, err
 		}
 	}
+}
+
+func extractError(response interface{}, err error) error {
+	if err != nil {
+		return err
+	}
+
+	if res, ok := response.(endpoint.Failer); ok {
+		return res.Failed()
+	}
+
+	return nil
 }
