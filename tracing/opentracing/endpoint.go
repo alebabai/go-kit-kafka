eab7a30f7d2156a7edf531ef98a2a592dc6883dd
Alexander Babai <aliaksandr.babai@gmail.com>
2021-03-22 18:45:36 +0300

    feat(tracing.opentracing): options support

--- tracing/opentracing/endpoint.go
+++ tracing/opentracing/endpoint.go
@@ -9,22 +9,43 @@ import (
 	"github.com/go-kit/kit/endpoint"
 )
 
-func TraceConsumer(tracer opentracing.Tracer, operationName string) endpoint.Middleware {
+func TraceEndpoint(tracer opentracing.Tracer, operationName string, opts ...EndpointOption) endpoint.Middleware {
+	cfg := &EndpointOptions{
+		Tags: make(opentracing.Tags),
+	}
+
+	for _, opt := range opts {
+		opt(cfg)
+	}
+
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (interface{}, error) {
-			span := opentracing.SpanFromContext(ctx)
-			if span == nil {
-				span = tracer.StartSpan(operationName)
+			if cfg.GetOperationName != nil {
+				if newOperationName := cfg.GetOperationName(ctx, operationName); newOperationName != "" {
+					operationName = newOperationName
+				}
+			}
+
+			var span opentracing.Span
+			if parentSpan := opentracing.SpanFromContext(ctx); parentSpan != nil {
+				span = tracer.StartSpan(
+					operationName,
+					opentracing.ChildOf(parentSpan.Context()),
+				)
 			} else {
-				span.SetOperationName(operationName)
+				span = tracer.StartSpan(operationName)
 			}
 			defer span.Finish()
-
-			ext.SpanKindConsumer.Set(span)
 			ctx = opentracing.ContextWithSpan(ctx, span)
 
+			applyTags(span, cfg.Tags)
+			if cfg.GetTags != nil {
+				extraTags := cfg.GetTags(ctx)
+				applyTags(span, extraTags)
+			}
+
 			response, err := next(ctx, request)
-			if err := extractError(response, err); err != nil {
+			if err := identifyError(response, err, cfg.IgnoreBusinessError); err != nil {
 				ext.LogError(span, err)
 			}
 
@@ -33,13 +54,29 @@ func TraceConsumer(tracer opentracing.Tracer, operationName string) endpoint.Mid
 	}
 }
 
-func extractError(response interface{}, err error) error {
+func TraceConsumer(tracer opentracing.Tracer, operationName string, opts ...EndpointOption) endpoint.Middleware {
+	opts = append(opts, WithTags(map[string]interface{}{
+		ext.SpanKindConsumer.Key: ext.SpanKindConsumer.Value,
+	}))
+
+	return TraceEndpoint(tracer, operationName, opts...)
+}
+
+func applyTags(span opentracing.Span, tags opentracing.Tags) {
+	for key, value := range tags {
+		span.SetTag(key, value)
+	}
+}
+
+func identifyError(response interface{}, err error, ignoreBusinessError bool) error {
 	if err != nil {
 		return err
 	}
 
-	if res, ok := response.(endpoint.Failer); ok {
-		return res.Failed()
+	if !ignoreBusinessError {
+		if res, ok := response.(endpoint.Failer); ok {
+			return res.Failed()
+		}
 	}
 
 	return nil
