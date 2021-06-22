f0c50262bbccdb1f4a44146c2684776de229423a
Alexander Babai <aliaksandr.babai@gmail.com>
2021-06-22 16:30:23 +0300

    refactor(kafka.tracing): consts naming

--- kafka/tracing/context.go
+++ kafka/tracing/context.go
@@ -9,15 +9,15 @@ import (
 type kafkaContextKey string
 
 const (
-	topicKey     kafkaContextKey = "kafka.topic"
-	partitionKey kafkaContextKey = "kafka.partition"
-	offsetKey    kafkaContextKey = "kafka.offset"
+	keyTopic     kafkaContextKey = "kafka.topic"
+	keyPartition kafkaContextKey = "kafka.partition"
+	keyOffset    kafkaContextKey = "kafka.offset"
 )
 
 func MessageToContext(ctx context.Context, msg *kafka.Message) context.Context {
-	ctx = context.WithValue(ctx, topicKey, msg.Topic)
-	ctx = context.WithValue(ctx, partitionKey, msg.Partition)
-	ctx = context.WithValue(ctx, offsetKey, msg.Offset)
+	ctx = context.WithValue(ctx, keyTopic, msg.Topic)
+	ctx = context.WithValue(ctx, keyPartition, msg.Partition)
+	ctx = context.WithValue(ctx, keyOffset, msg.Offset)
 
 	return ctx
 }
@@ -25,16 +25,16 @@ func MessageToContext(ctx context.Context, msg *kafka.Message) context.Context {
 func ContextToTags(ctx context.Context) map[string]interface{} {
 	tags := make(map[string]interface{})
 
-	if topic := ctx.Value(topicKey); topic != nil {
-		tags[string(topicKey)] = topic
+	if topic := ctx.Value(keyTopic); topic != nil {
+		tags[string(keyTopic)] = topic
 	}
 
-	if partition := ctx.Value(partitionKey); partition != nil {
-		tags[string(partitionKey)] = partition
+	if partition := ctx.Value(keyPartition); partition != nil {
+		tags[string(keyPartition)] = partition
 	}
 
-	if offset := ctx.Value(offsetKey); offset != nil {
-		tags[string(offsetKey)] = offset
+	if offset := ctx.Value(keyOffset); offset != nil {
+		tags[string(keyOffset)] = offset
 	}
 
 	return tags
