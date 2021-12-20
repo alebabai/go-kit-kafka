860e1607fa281ab9033d3a8a48ae247bb2d11b84
Alexander Babai <aliaksandr.babai@gmail.com>
2021-12-21 01:32:46 +0300

    refactor(kafka.transport): simplify router
    use type alias with extra methods

--- kafka/transport/router.go
+++ kafka/transport/router.go
@@ -7,55 +7,16 @@ import (
 	"github.com/alebabai/go-kit-kafka/kafka"
 )
 
-// Handlers represents Topic -> []Handler mapping
-type Handlers map[string][]kafka.Handler
+type Router map[string][]kafka.Handler
 
-type Router struct {
-	handlers Handlers
-}
-
-func NewRouter(opts ...RouterOption) *Router {
-	r := &Router{
-		handlers: make(Handlers),
-	}
-
-	for _, opt := range opts {
-		opt(r)
-	}
+func (r Router) AddHandler(topic string, handler kafka.Handler) Router {
+	r[topic] = append(r[topic], handler)
 
 	return r
-}
-
-type RouterOption func(*Router)
-
-func RouterWithHandler(topic string, handler kafka.Handler) RouterOption {
-	return func(r *Router) {
-		r.AddHandler(topic, handler)
-	}
-}
-
-func RouterWithHandlers(handlers Handlers) RouterOption {
-	return func(r *Router) {
-		r.handlers = handlers
-	}
-}
-
-func (r *Router) AddHandler(topic string, handler kafka.Handler) *Router {
-	if len(r.handlers) == 0 {
-		r.handlers = make(Handlers)
-	}
-
-	r.handlers[topic] = append(r.handlers[topic], handler)
-
-	return r
-}
-
-func (r Router) Handlers() Handlers {
-	return r.handlers
 }
 
 func (r Router) Handle(ctx context.Context, msg *kafka.Message) error {
-	for _, h := range r.handlers[msg.Topic] {
+	for _, h := range r[msg.Topic] {
 		if err := h.Handle(ctx, msg); err != nil {
 			return fmt.Errorf("failed to handle message from kafka topic=%s: %w", msg.Topic, err)
 		}
